1fa13f5fa7fd8650e85e44a81a057622f706d141
Oliver Hader <oliver@typo3.org>
2017-04-26 18:53:40 +0200

    [TASK] Release v1.7.39 (based on v1.7.3)

--- dist/fabric.js
+++ dist/fabric.js
@@ -1,7 +1,7 @@
 /* build: `node build.js modules=ALL exclude=json,gestures minifier=uglifyjs` */
  /*! Fabric.js Copyright 2008-2015, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "1.7.999" };
+var fabric = fabric || { version: "1.7.39" };
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
 }
